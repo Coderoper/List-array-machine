ad1fb1dd6077f29e708a04bc6d2abade08ccead5
andrew lupton and Austin Roberts <andrew@>
2018-03-26 16:36:15 -0700

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,16 @@ $(document).ready(function(){
   $("form#blanks").submit(function(event) {
     event.preventDefault();
     $(".blank").text("");
-    // array.push(favoriteItem1);
-    // array.push(favoriteItem2);
+
     $("#result").append("<li>" + " " + "</li>")
     var blanks = ["favoriteItem1", "favoriteItem2"];
     blanks.forEach(function(blank)
     {
       var userInput = $("input#" + blank).val();
-      $("#result").append(" " + userInput + " " );
-      // $("#result").append();
+      $("#result").append(" " + userInput + "");
     });
 
-      // $("<span>").attr("class", "blank").text(userInput)
-
-      // )
     $("#result").show();
-
-    // $("ul#result").append(array);
-    // $("li#answer").text(array);
-    // $("span#answer").text(favoriteItem2);
-  //  $("ul#result").append(array);
-      // $("span#answer").text(array);
 
   });
 
